03ec90553bd42dbd11bf38d2fdad655342b00c88
Gabriel de Rezende Gonçalves <gabireze@gmail.com>
2021-04-14 20:56:54 -0300

    applying date within the order

--- infrastructure/tables.js
+++ infrastructure/tables.js
@@ -5,7 +5,7 @@ class Tables {
   };
 
   createServices() {
-    const sql = 'CREATE TABLE IF NOT EXISTS Services (id int NOT NULL AUTO_INCREMENT, client varchar(50) NOT NULL, pet varchar(20), service varchar(20) NOT NULL, status varchar(20) NOT NULL, comment text, PRIMARY KEY(id))';
+    const sql = 'CREATE TABLE IF NOT EXISTS Services (id int NOT NULL AUTO_INCREMENT, client varchar(50) NOT NULL, pet varchar(20), service varchar(20) NOT NULL, date datetime NOT NULL, createdAt datetime NOT NULL, status varchar(20) NOT NULL, comment text, PRIMARY KEY(id))';
     this.connection.query(sql, error => {
       if (error) {
         console.log(error);
--- services/order.js
+++ services/order.js
@@ -1,10 +1,14 @@
+const moment = require('moment');
 const OrderDAO = require('../daos/order');
 const Order = require('../models/order');
 
 class OrderService {
   async insert(body) {
     const order = new Order(body);
-    const result = await OrderDAO.insert(order);
+    const createdAt = moment().format('YYYY-MM-DD HH:MM:SS');
+    const date = moment(body.date, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+    const datedOrder = { ...order, createdAt, date };
+    const result = await OrderDAO.insert(datedOrder);
   };
 };
 
