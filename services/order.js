815dc064f7dd537b50d6673899f08226efcd5951
Gabriel de Rezende Gonçalves <gabireze@gmail.com>
2021-04-14 21:48:18 -0300

    applying input validations

--- services/order.js
+++ services/order.js
@@ -7,8 +7,33 @@ class OrderService {
     const order = new Order(body);
     const createdAt = moment().format('YYYY-MM-DD HH:MM:SS');
     const bodyDate = moment(body.date, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
-    const datedOrder = { ...order, createdAt, date: bodyDate };
-    const result = await OrderDAO.insert(datedOrder, response);
+
+    const validatedDate = moment(bodyDate).isSameOrAfter(createdAt);
+    const validatedClient = body.client.length >= 5;
+
+    const validations = [
+      {
+        name: 'date',
+        valid: validatedDate,
+        message: 'Data deve ser maior ou igual a data atual'
+      },
+      {
+        name: 'client',
+        valid: validatedClient,
+        message: 'O nome do cliente deve conter 5 ou mais caracteres'
+      }
+    ];
+
+    const errors = validations.filter(field => !field.valid);
+    const isNotValid = errors.length
+
+    if (isNotValid) {
+      response.status(400).json(errors);
+    } else {
+      const datedOrder = { ...order, createdAt, date: bodyDate };
+      const result = await OrderDAO.insert(datedOrder, response);
+    }
+
   };
 };
 
